cfdf95127a79ebdeabe9c5e93bcdfc74b51d3071
Keagan <keaganchs@gmail.com>
2025-02-24 16:05:39 +0100

    Update config

--- train_scripts/slurm_pmc_train.sh
+++ train_scripts/slurm_pmc_train.sh
@@ -5,9 +5,9 @@
 #SBATCH --error=log/out_and_err_%j.txt
 #SBATCH --partition=stud
 #SBATCH --nodes=1
-#SBATCH --ntasks=7
+#SBATCH --ntasks=5
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=200
+#SBATCH --mem-per-cpu=2000
 #SBATCH --time=23:59:59
 
 #SBATCH --gres=gpu:1
@@ -16,7 +16,7 @@ eval "$(~/miniconda3/bin/conda shell.bash hook)"
 conda activate lifelike
 
 export CONTROL_FREQ=50
-export PORT_OFFSET=5
+export PORT_OFFSET=0
 export USE_TORQUE_ACTIONS=False
 export GAMMA=0.95
 
@@ -24,8 +24,8 @@ export WANDB_ENABLE_TRACKING=True
 export WANDB_ENTITY=keagan
 export WANDB_PROJECT=lifelike_agility_and_play
 export WANDB_GROUP=phase_1
-export WANDB_NAME=pd_control_50_hz
-export WANDB_NOTES="Test different training frequency"
+export WANDB_NAME=pd_control_50_hz_8192_batch_size
+export WANDB_NOTES="Increase batch size to 8192"
 
 # Note: num_actors must be >= num_learners, but usually one actor per learner is sufficient as the bottleneck is the learner's processing power.
 # Note: ntasks must be manually defined. Assuming one actor per learner, ntasks=3+num_learners (1 game_mgr, 1 actor, 1 learner, num_learners actors).
