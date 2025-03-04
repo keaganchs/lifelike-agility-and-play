7e2cb5867cf52fb6122574c4f12435f139dc3506
Keagan <keaganchs@gmail.com>
2025-03-04 13:31:07 +0100

    Update test script

--- train_scripts/test_single_pmc_train.sh
+++ train_scripts/test_single_pmc_train.sh
@@ -1,14 +1,15 @@
 export CONTROL_FREQ=50
-export PORT_OFFSET=5
+export PORT_OFFSET=0
 export USE_TORQUE_ACTIONS=False
 export GAMMA=0.95
 
 export WANDB_ENABLE_TRACKING=False
+export WANDB_NAME="asdafkjahdkasj"
 
 # Note: num_actors must be >= num_learners, but usually one actor per learner is sufficient as the bottleneck is the learner's processing power.
 # Note: ntasks must be manually defined. Assuming one actor per learner, ntasks=3+num_learners (1 game_mgr, 1 actor, 1 learner, num_learners actors).
 # Node: make sure to update pmc.conf to reflect the number of tasks, and the environment variable for each actor.
-export NUM_LEARNERS=2
+export NUM_LEARNERS=1
 
 
 # Map 1-4 to the respective roles for convenience, while still allowing names
